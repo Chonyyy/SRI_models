a3472022c921c2464b41c0224da77680b4a6408d
aspio28 <aspiolea28@gmail.com>
2024-03-05 01:40:37 -0500

    generalize model

--- src/code/text_processor.py
+++ src/code/text_processor.py
@@ -2,6 +2,8 @@ import ir_datasets
 import nltk
 import gensim
 from query import Query
+import numpy as np
+import math
 
 class TextProcessor:
     def __init__(self):
@@ -9,12 +11,16 @@ class TextProcessor:
         self.corpus = []
         dataset = ir_datasets.load("cranfield")
         self.docs = [doc.text for doc in dataset.docs_iter()]
+        del self.docs[470]
+        del self.docs[993]
         self.tokenized_docs = self.filter_tokens_by_occurrence(self.morphological_reduction_nltk(self.remove_stopwords(self.remove_noise_nltk(self.tokenization_nltk()))))
+        self.vocabulary = list(self.dictionary.token2id.keys())
         self.vector_repr = self.vector_representation(self.tokenized_docs, self.dictionary)
+        # self.vector_repr = self.generalize(self.vector_repr)
 
     def tokenization_nltk(self):
         #Tokenize the query using NLTK
-        tokenized = [nltk.tokenize.word_tokenize(doc) for doc in self.docs[:10]]
+        tokenized = [nltk.tokenize.word_tokenize(doc) for doc in self.docs]
         return tokenized
          
     def remove_noise_nltk(self, tokenized_docs):
@@ -41,18 +47,17 @@ class TextProcessor:
 
         return tokenized_docs
 
-    def filter_tokens_by_occurrence(self, tokenized_docs, no_below=2, no_above=0.5):
+    def filter_tokens_by_occurrence(self, tokenized_docs, no_below=5, no_above=0.5):
         if not self.dictionary:
             self.dictionary = gensim.corpora.Dictionary(tokenized_docs)
             self.dictionary.filter_extremes(no_below=no_below, no_above=no_above)
-     
         filtered_words = [word for _, word in self.dictionary.iteritems()]
         return [
             [word for word in doc if word in filtered_words]
             for doc in tokenized_docs
         ]
     
-    def vector_representation(self, tokenized_docs, dictionary, use_bow=False):
+    def vector_representation(self, tokenized_docs, dictionary, use_bow=True):
         if not self.corpus:
             self.corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
 
@@ -64,7 +69,55 @@ class TextProcessor:
 
         return vector_repr
     
-    def query_processor(self, query, use_bow=False):
+    def generate_independets_vectors(self):
+        # Genera un vector inicial con 1 en la primera posición y 0 en el resto
+        vector_inicial = [1] + [0] * (len(self.vector_repr) - 1)
+        
+        # Genera la lista de vectores rotando el 1 a través de ellos
+        lista_de_vectores = [vector_inicial[-i:] + vector_inicial[:-i] for i in range(len(self.vector_repr))]
+        
+        return np.array(lista_de_vectores)
+    
+    def generalize(self, vectors_weight, query=False):
+        c = [[0] * len(self.vector_repr) for _ in range(len(self.vocabulary))]
+
+        vectors = self.generate_independets_vectors()
+
+        for i, k in zip(self.vector_repr, range(len(self.vector_repr))):
+            for j in i:
+                c[j[0]][k] += j[1]
+
+        k_i = []
+        k = 0
+        for i in c:
+            c_big = 0
+            for j,v in zip(i,vectors):
+                k = k + j * v
+                c_big += j ** 2
+            k_i.append((k/math.sqrt(c_big)).tolist())
+            k = 0
+            c_big = 0
+
+        new_vectors = []
+        
+        if query:
+            for vector in (vectors_weight):
+                new_weight = 0
+                for token in k_i[vector[0]]:  
+                    new_weight += vector[1] * token          
+                new_vectors.append((vector[0], new_weight))
+            
+        else:    
+            for doc in range(len(vectors_weight)):
+                new_tuple = []
+                for vector in vectors_weight[doc]:
+                    new_tuple.append((vector[0], k_i[vector[0]][doc] * vector[1]))
+                new_vectors.append(new_tuple)
+        
+        return new_vectors
+
+
+    def query_processor(self, query, use_bow=True):
         self.query_processed = Query(query)
         self.query_processed.process_query()
 
@@ -79,6 +132,8 @@ class TextProcessor:
             tfidf = gensim.models.TfidfModel(self.corpus)
             self.query_processed.vector_repr = tfidf[self.query_processed.query_bow]
 
+        # self.query_processed.vector_repr = self.generalize(self.query_processed.vector_repr, query=True)
+
     def similarity(self):
         #Find matched documents based on the query
         index = gensim.similarities.MatrixSimilarity(self.vector_repr)
@@ -88,3 +143,13 @@ class TextProcessor:
 
         best_match_indices = [match[0] for match in top_matches]
         return best_match_indices
+    
+proc = TextProcessor()
+
+# print('akkakaka')
+# print('-----------------------------')
+# proc.query_processor('free-stream mach number and the impact and free-stream pressures and densities for extremely low pressures .it is shown that the results differ appreciably from the corresponding continuum relations')
+
+# print(proc.similarity())
+
+# print(proc.docs[969])
